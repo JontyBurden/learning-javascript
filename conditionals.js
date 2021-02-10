16ef4d3dfd5f6925becc02783d1fb3749a44c966
JontyBurden <azttorrs@gmail.com>
2021-02-10 20:30:15 +1300

    learning about if and else if statements

---
+++ conditionals.js
@@ -0,0 +1,9 @@
+let age = 65;
+
+if (age < 18) {
+    console.log("You can't drink in New Zealand")
+} else if (age >= 65) {
+    console.log("Drink in moderatoin")
+} else {
+        console.log("You can drink in New Zealand")
+}
